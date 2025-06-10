e6e40169f2ce23a6196b711e1486c9b0579f3524
hrabovyiserhii <103687785+hrabovyiserhii@users.noreply.github.com>
2025-06-10 19:08:42 +0300

    My commit message

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { Truck, Leaf, Award, ArrowRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div data-component="HomePage" className="min-h-screen flex flex-col">
+      {" "}
+      JUST TEST{" "}
+    </div>
+  );
+}
